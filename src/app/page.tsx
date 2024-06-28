4420235c26ca0ae37f715aa149dcd08585ea0fc1
Naimur Rahman <naimur2231@gmail.com>
2024-06-29 04:01:28 +0600

    Skills section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import PublicLayout from "@/components/layout/PublicLayout";
 import About from "@/components/sections/Home/About/About";
+import Skills from "@/components/sections/Home/Skills/Skills";
 import SuccessWork from "@/components/sections/Home/SuccessWork/SuccessWork";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <PublicLayout>
         <SuccessWork />
         <About />
+        <Skills />
       </PublicLayout>
     </main>
   );
